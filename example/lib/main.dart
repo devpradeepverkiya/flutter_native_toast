ac8e233a71d769c7177bbf2b9c354059083414c5
Pradeep Kumar <pradeepkmr@synapseco.com>
2021-02-06 14:09:19 +0530

    update plugin

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,35 +13,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
 
   @override
   void initState() {
     super.initState();
-    //initPlatformState();
-    FlutterToast.show_toast("Hi how are you");
+    FlutterToast.show_toast("Flutter Native Toast Example");
   }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterToast.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
 
 
   @override
@@ -49,10 +26,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          title: const Text('Flutter Native Toast'),
         ),
       ),
     );
--- lib/flutter_toast.dart
+++ lib/flutter_toast.dart
@@ -1,16 +1,7 @@
-import 'dart:async';
-
 import 'package:flutter/services.dart';
 
 class FlutterToast {
   static const MethodChannel _channel = const MethodChannel('flutter_toast');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion',{"Version_name ":"joy"});
-    return version;
-  }
-  
-
   static show_toast(String msg) {
     _channel.invokeMethod('show_toast', {"msg": msg});
   }
